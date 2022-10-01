10e0e82d5165ad325b07876b9da6c035f94d9cf6
Sinduy <sjsanjsrh@naver.com>
2022-10-01 13:11:21 +0000

    add remove script

---
+++ WEB_BE/remove.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# This script is used to remove sgc_musago from the server
+
+if [ "`id -u`" -ne 0 ]; then
+echo "please run sudo"
+exit
+fi
+
+set -e
+set -v
+
+# Get the path to the script
+HERE=$(dirname $(realpath $0))
+
+# Set the environment variables
+WEB_BE_HOME=/usr/local/etc/sgc_musago/WEB_BE
+WEB_FE_HOME=/usr/local/etc/sgc_musago/WEB_FE
+ROOT_HOME=/usr/local/etc/sgc_musago
+SERVICE_FILE=/etc/systemd/system/sgc_musago.service
+
+# Stop the service
+systemctl stop sgc_musago
+
+# Disable the service
+systemctl disable sgc_musago
+
+# Remove the service file
+if [ -e $SERVICE_FILE ]; then
+  rm -f $SERVICE_FILE
+fi
+
+# Remove the directories
+rm -rf $WEB_BE_HOME
+rm -rf $WEB_FE_HOME
+if [ -e $ROOT_HOME ]; then
+  rm -rf $ROOT_HOME
+fi
+
+# Reload the systemd daemon
+systemctl daemon-reload
