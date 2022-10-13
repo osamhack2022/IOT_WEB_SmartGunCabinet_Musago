f4cbd3c52f26280351ffd18077793c6cde3341b6
makerswr <alceoswr@gmail.com>
2022-10-13 17:36:30 +0900

    RPi Arduino serial test

---
+++ IoT(Raspberry Pi)/dataReceiveTest.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import serial
+
+GPIO.setmode(GPIO.BOARD)
+
+
+port = "/dev/ttyACM0"
+serial = serial.Serial(port)
+serial.flushInput()
+
+while True:
+	if(serial.in_waiting > 0):
+		sensData = serial.readline()
+		print(sensData)
+	
